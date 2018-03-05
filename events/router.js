de3f5ca0a81a41090a27c6bb851e29a1adac5261
Rishitesh87 <roepnarainr@gmail.com>
2018-03-05 07:39:19 +0100

    Update and destroy event

--- events/router.js
+++ events/router.js
@@ -60,59 +60,59 @@ router.post('/events', (req, res) => {
     })
 })
 
-// const updateOrPatch = (req, res) => {
-//   const eventId = Number(req.params.id)
-//   const updates = req.body
-//
-//   Event.findById(req.params.id)
-//     .then(entity => {
-//       if (entity.userId !== req.user.id) {
-//         res.status(403).send({
-//           message: 'You\'re not allowed to edit this event!'
-//         })
-//       }
-//       else {
-//         return entity.update(updates)
-//       }
-//     })
-//     .then(final => {
-//       res.json(final)
-//     })
-//     .catch(error => {
-//       res.status(500).send({
-//         message: `Something went wrong`,
-//         error
-//       })
-//     })
-// }
-//
-// router.put('/events/:id', updateOrPatch)
-// router.patch('/events/:id', updateOrPatch)
-//
-// router.delete('/events/:id', (req, res) => {
-//   Product.findById(req.params.id)
-//     .then(entity => {
-//       if (entity.userId !== req.user.id) {
-//         res.status(403).send({
-//           message: 'You\'re not allowed to delete this event!'
-//         })
-//       }
-//       else {
-//         return entity.destroy()
-//       }
-//     })
-//     .then(_ => {
-//       res.send({
-//         message: 'The event was deleted succesfully'
-//       })
-//     })
-//     .catch(error => {
-//       res.status(500).send({
-//         message: `Something went wrong`,
-//         error
-//       })
-//     })
-// })
+const updateOrPatch = (req, res) => {
+  const eventId = Number(req.params.id)
+  const updates = req.body
+
+  Event.findById(req.params.id)
+    .then(entity => {
+      if (entity.userId !== req.user.id) {
+        res.status(403).send({
+          message: 'You\'re not allowed to edit this event!'
+        })
+      }
+      else {
+        return entity.update(updates)
+      }
+    })
+    .then(final => {
+      res.json(final)
+    })
+    .catch(error => {
+      res.status(500).send({
+        message: `Something went wrong`,
+        error
+      })
+    })
+}
+
+router.put('/events/:id', updateOrPatch)
+router.patch('/events/:id', updateOrPatch)
+
+router.delete('/events/:id', (req, res) => {
+  Product.findById(req.params.id)
+    .then(entity => {
+      if (entity.userId !== req.user.id) {
+        res.status(403).send({
+          message: 'You\'re not allowed to delete this event!'
+        })
+      }
+      else {
+        return entity.destroy()
+      }
+    })
+    .then(_ => {
+      res.send({
+        message: 'The event was deleted succesfully'
+      })
+    })
+    .catch(error => {
+      res.status(500).send({
+        message: `Something went wrong`,
+        error
+      })
+    })
+})
 
 
 
